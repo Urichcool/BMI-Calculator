2f009f1cc4afa86cfecd5842bff845978706b907
Yurii Novikov <urichcoolx@gmail.com>
2023-07-26 21:50:50 +0200

    finished-header-background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './scss/index.scss';
+import HeroHeader from './components/Hero/HeroHeader';
 
 function App() {
   return (
     <div className="App">
+      <HeroHeader/>
     </div>
   );
 }
